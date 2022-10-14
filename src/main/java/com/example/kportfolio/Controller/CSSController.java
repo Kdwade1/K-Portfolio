e2107978b468d043ebe57824feb0760e4c4fc7a8
Kelvin <kelvin.daquon.wade@gmail.com>
2022-10-13 19:57:29 -0500

    updated while sick

--- src/main/java/com/example/kportfolio/Controller/CSSController.java
+++ src/main/java/com/example/kportfolio/Controller/CSSController.java
@@ -1,4 +1,12 @@
 package com.example.kportfolio.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CSSController {
+    @GetMapping("/css")
+    public String showcss(){
+        return"css1";
+    }
 }
