eae8809a9ad43cefa2774a32f90375001ac17b13
Kelvin <kelvin.daquon.wade@gmail.com>
2022-10-14 17:02:24 -0500

    update

--- src/main/java/com/example/kportfolio/Controller/CSSController.java
+++ src/main/java/com/example/kportfolio/Controller/CSSController.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CSSController {
     @GetMapping("/css")
-    public String showcss(){
+    public String showCss(){
         return"css1";
+    }
+    @GetMapping("/css2")
+    public String showCss2(){
+        return "css2";
+    }
+    @GetMapping("/css3")
+    public String showCss3() {
+        return "css3";
     }
 }
