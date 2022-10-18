9321f342960bdab0f9cb3d1a1143d874e7f5e29a
Kelvin <kelvin.daquon.wade@gmail.com>
2022-10-18 18:22:44 -0500

    update

--- src/main/java/com/example/kportfolio/Controller/JavaScriptController.java
+++ src/main/java/com/example/kportfolio/Controller/JavaScriptController.java
@@ -1,4 +1,10 @@
 package com.example.kportfolio.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class JavaScriptController {
+    @GetMapping("/javascript")
+    public String showJs(){
+        return"javascript1";
+    }
 }
