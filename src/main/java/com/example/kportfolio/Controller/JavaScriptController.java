b6d998e5a8c5788c1eedfb437ac2ba8ddc177a0f
Kelvin <kelvin.daquon.wade@gmail.com>
2022-10-18 19:03:06 -0500

    update

--- src/main/java/com/example/kportfolio/Controller/JavaScriptController.java
+++ src/main/java/com/example/kportfolio/Controller/JavaScriptController.java
@@ -1,7 +1,8 @@
 package com.example.kportfolio.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class JavaScriptController {
     @GetMapping("/javascript")
     public String showJs(){
