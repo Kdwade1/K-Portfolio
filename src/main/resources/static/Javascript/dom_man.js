11cfe26f33abf0199e56f56a1b381d1765977768
Kelvin <kelvin.daquon.wade@gmail.com>
2022-11-17 23:32:33 -0600

    update

--- src/main/resources/static/Javascript/dom_man.js
+++ src/main/resources/static/Javascript/dom_man.js
@@ -4,3 +4,11 @@ const checkbox= document.getElementById("checkbox")
 checkbox.addEventListener('change',()=>{
 document.body.classList.toggle('dark')
 })
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
