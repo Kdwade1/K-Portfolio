d91f6300583c6e35fcab221c934fe5e04b0474f3
Kelvin <kelvin.daquon.wade@gmail.com>
2022-12-05 21:56:38 -0600

    updated post functionailty

--- src/main/java/com/example/kportfolio/Controller/JavaController.java
+++ src/main/java/com/example/kportfolio/Controller/JavaController.java
@@ -9,12 +9,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Controller
 public class JavaController {
-    private final UserRepository userDao ;
+    private final UserRepository userDao;
     private final JPostRepository JPostDao;
     @Value("AVHQNuSVLSPK8S5AWyXa2z")
     private String fileApi;
@@ -25,31 +29,45 @@ public class JavaController {
     }
 
     @GetMapping("/java")
-    public String viewJava(Model model){
-        List<JPost> all =JPostDao.findAll();
-        model.addAttribute("Jpost",all);
-        try{
-            User currentUser=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            model.addAttribute("currentUser",currentUser);
+    public String viewJava(Model model) {
+        List<JPost> all = JPostDao.findAll();
+        model.addAttribute("Jpost", all);
+        try {
+            User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("currentUser", currentUser);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             return "java";
         }
-        return"java";
+        return "java";
 
-        }
+    }
 
 
+    @GetMapping("/jPost/show/{id}")
+    public String showPostId(@PathVariable long id, Model model) {
+        model.addAttribute("post", JPostDao.getById(id));
+        return "jPost/show";
 
+    }
 
 
     @GetMapping("/JPost/create")
-    public String showCreationPost(Model model){
-        model.addAttribute("jpost",new JPost());
-        model.addAttribute("fileApi",fileApi);
+    public String showCreationPost(Model model) {
+        model.addAttribute("jpost", new JPost());
+        model.addAttribute("fileApi", fileApi);
 
-        return"create-JPost";}
+        return "create-JPost";
+    }
 
+    @PostMapping("JPost/create")
+    public String createPost(@ModelAttribute JPost post) throws ParseException {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        post.setUser(currentUser);
+        JPostDao.save(post);
+        return "redirect:/java";
+
+    }
 
 
 }
